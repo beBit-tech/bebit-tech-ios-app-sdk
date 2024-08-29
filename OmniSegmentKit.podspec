ef7458104e2c197595230076a4625c3e33cfa741
Kalle Chen <kallechen.tw@gmail.com>
2024-08-29 20:39:49 +0800

    Release 1.0.0-beta.1

--- OmniSegmentKit.podspec
+++ OmniSegmentKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name                   = "OmniSegmentKit"
-  spec.version                = "1.0.0-beta.0"
+  spec.version                = "1.0.0-beta.1"
   spec.authors                = "Lewis"
   spec.summary                = "beBit-Tech App Popup SDK."
   spec.homepage               = "https://github.com/beBit-tech/bebit-tech-ios-app-sdk"
--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
 		),
 		.binaryTarget(
 			name: "OmniSegmentKit",
-			url: "https://github.com/beBit-tech/bebit-tech-ios-app-sdk/releases/download/1.0.0-beta.0/OmniSegmentKit.xcframework.zip",
-			checksum: "2ae6536f64287301a923c01b3b359a39c5831dac391392c66d9606361c287632"
+			url: "https://github.com/beBit-tech/bebit-tech-ios-app-sdk/releases/download/1.0.0-beta.1/OmniSegmentKit.xcframework.zip",
+			checksum: "35eba99cdb6f4683f771de4e96c2607fffa7ab24fe984360741e3e00538b4df5"
 		)
 	]
 )
